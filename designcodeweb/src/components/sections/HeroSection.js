a8414dd65eb89c40b54daba5e3162887bfb76d34
anandb7248 <anandb7248@gmail.com>
2021-04-04 11:21:48 -0700

    complete module 9

--- designcodeweb/src/components/sections/HeroSection.js
+++ designcodeweb/src/components/sections/HeroSection.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import { H1, MediumText } from "../styles/TextStyles"
 import { themes } from "../styles/ColorStyles"
 import PurchaseButton from "../buttons/PurchaseButton"
+import MockUpAnimation from "../animations/MockUpAnimation"
 
 export default function HeroSection() {
   return (
@@ -22,6 +23,7 @@ export default function HeroSection() {
             subtitle="120+ hours of video"
           />
         </TextWrapper>
+        <MockUpAnimation />
       </ContentWrapper>
     </Wrapper>
   )
@@ -29,12 +31,15 @@ export default function HeroSection() {
 
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+  overflow: hidden;
 `
 
 const ContentWrapper = styled.div`
   max-width: 1234px;
   margin: 0 auto;
   padding: 200px 30px;
+  display: grid;
+  grid-template-columns: 360px auto;
 `
 
 const TextWrapper = styled.div`
